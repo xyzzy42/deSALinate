77314a7b8c937289bab09321464cea4f295b32e6
Trent Piepho <tpiepho@gmail.com>
2026-08-31 21:37:42 +0000

    Fixup cast decoding

--- amb.py
+++ amb.py
@@ -598,7 +598,7 @@ def decode(strings:list[str], data:bytes, loc=0x0c50,
             p2 = data[i+2]
             v2 = data[i+3]
             i += 4
-            pr(f"Cast usr {v:02x} from {PoSs[p1]} to {PoSs[p2]}")
+            pr(f"Cast usr {v:02x} from {PoSs[p1]} to {PoSs[p2] if p2 < len(PoSs) else "??"}")
 
         elif op == OpA.parse:
             i += 39 # Seems like it's this big?
